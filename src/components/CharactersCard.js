89fbabc6a995d06f18380f17b727ac99f042023d
Dandara Almeida <dandara.avazquez@gmail.com>
2021-03-05 14:15:23 +0100

    Update style

--- src/components/CharactersCard.js
+++ src/components/CharactersCard.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 
 const Characters = props => {
-  
   return (
     <Link to={`/character/${props.character.id}`}>
       <img className="card__img" src={props.character.image} alt="Foto personaje" />
